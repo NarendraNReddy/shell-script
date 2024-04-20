42a917d21924f6ee5c269f90f889307e23716b93
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 14:08:34 -0700

    02-variables.script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::Hi Suresh,how are you"
+echo "Suresh:: Hi Ramesh, i am fine.Thank you. How are you"
+echo "Ramesh:: i m good.how about your office"
+echo "Suresh:: good. Planning to take devops course"
