e799c9c8d103a9a435eda71a5180814957389a90
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 14:25:19 -0700

    03_a_variables.script

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=SURESH
+PERSON1=SACHIN
+PERSON2=RAGHU
 
 echo "$PERSON1::Hi $PERSON2,how are you"
 echo "$PERSON2:: Hi $PERSON1, i am fine.Thank you. How are you"
