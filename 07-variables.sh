69d2ff1f94b69e2e70dffc43d98e8c7d79ecd4fa
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 15:04:26 -0700

    07_datatypes

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-#x=10
-#y=20
-#echo "x=10, y=5"  
-#echo "Addition of x and y"  
-
-#echo $(( $x + $y ))  
-
-
-
 NO1=$1
-NO2=$2
+NO2=$2 
 
-sum=$(( $NO1 + $NO2 ))
+sum=(( $NO1 + $NO2 ))
 echo $sum
