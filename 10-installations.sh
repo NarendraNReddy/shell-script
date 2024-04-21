6cb4cc08b8c5202521a14f46c77ec65e0c21fcea
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 18:54:18 -0700

    10-installation

--- 10-installations.sh
+++ 10-installations.sh
@@ -10,5 +10,12 @@ else
 fi
 dnf install  mysql -y
 
+if [ $? -ne 0 ];
+then 
+    echo "Installation of mysql is ..FAILURE"
+    exit 1
+else 
+    echo "Installation of mysql is ... SUCCESS"
+fi        
 
 
