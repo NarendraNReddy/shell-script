013d43b3012ca9ea940129f9f404e06d6bcdd760
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 18:24:04 -0700

    09_conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NUMBER=10
+NUMBER=11
 if [ $NUMBER -gt 10 ];
 then 
-    echo "Number is less than 10"
-else
     echo "Number is greater than 10"
+else
+    echo "Number is less than 10"
 fi        
