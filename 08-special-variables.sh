fba07becb402b50acd586951bcedf2bb62205537
NarendraReddy <narendra.nya@gmail.com>
2024-04-20 18:09:07 -0700

    08_special

---
+++ 08-special-variables.sh
@@ -0,0 +1,13 @@
+#!/usr/bin
+echo "All cmd line args:$@"
+echo "No of cmd line:$#"
+echo "Prog name :$0"
+
+echo "username $USER"
+echo "Current dir:$PWD"
+echo "Home :$HOME"
+
+echo "Hostname :$HOSTNAME"
+
+echo "Pid of current program : $$"
+echo "Last background process:$!"
