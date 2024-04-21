00a2e8b8eb67054f2cc0e0f6d887d9ca58d6343b
NarendraReddy <narendra.nya@gmail.com>
2024-04-21 07:28:18 -0700

    14-installations

--- 14-installation.sh
+++ 14-installation.sh
@@ -2,7 +2,20 @@
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-echo $LOGFILE
+R="\e[31m"
+G="\e[32m"
+B="\e[34m"
+N="\e[0m"
+
+VALIDATE()
+{
+    if [ $? -ne 0 ];
+    then 
+        echo -e "$2 is....$R FAILURE $N"
+    else 
+        echo -e "$2 is...$G SUCCESS $N"
+    fi    
+}
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ];
@@ -10,5 +23,12 @@ then
     echo "Please be a super user"
 else
     echo "Super user"
-fi 
+fi
+
+
+for i in $@;
+do 
+    echo $i
+done 
+
 
