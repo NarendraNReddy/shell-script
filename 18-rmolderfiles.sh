f69c06f549a2d2659e4d9c9f4889e5c662e9bee9
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 05:48:19 -0700

    olderfiles

--- 18-rmolderfiles.sh
+++ 18-rmolderfiles.sh
@@ -19,5 +19,6 @@ FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
 while IFS= read -r line
 do
-    echo "Deleting file:$line"s
+    #echo "Deleting file:$line"
+    rm -rf $line
 done <<< $FILES
