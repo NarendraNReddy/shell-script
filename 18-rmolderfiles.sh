a19da5ae47e0df655149bce4f870c89b7e2cbe44
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 05:25:14 -0700

    olderfiles

---
+++ 18-rmolderfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIRECTORY="/tmp/app-logs"
+
+if [ -d $SOURCE_DIRECTORY ];
+then 
+    echo -e "$G Dir:$SOURCE_DIRECTORY exists $N\n"
+else 
+    echo -e "$R No dir:$SOURCE_DIRECTORY exists $N\n"   
+fi 
