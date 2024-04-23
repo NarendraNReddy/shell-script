a80e8a5aff6d67869417293675c163ab604ffa9e
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 02:49:54 -0700

    callingscripts

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -6,5 +6,6 @@ echo "Before calling other script ,course:$COURSE"
 echo "The PID of the current shell:$$"
 
 #./16-other.script.sh 
+source ./16-other-script.sh
 
 echo "After calling other script,course:$COURSE"
